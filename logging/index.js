830573c9a866f9b29ab6f4bd2270f1c591dbc6b4
Kevin Forbes <forbesk9@gmail.com>
2015-11-08 00:50:36 -0700

    Implemented funcs, need to clean up setLogDirectory

--- logging/index.js
+++ logging/index.js
@@ -3,53 +3,104 @@ var thrift = require("thrift");
 var Logger = require("./logservice/LogService");
 var ttypes = require("./logservice/LogService_types");
 
+var dir = __dirname;
 var file = false;
+var fileList = [];
+var initTime = 0;
+
+
+var getHrTimeMs = function() {
+  time = process.hrtime();
+  return time[0] * 1000 + time[1] / 1000000;
+}
+
+var updateLogFileList = function() {
+  fileList = fs.readdirSync(dir);
+}
 
 var LoggerHandler = {
-  startLogging: function(filename, result) {
-    if(!file) {
-        // <add filename verification here>
-        file = fs.createWriteStream(filename, {flags: 'w'});
-        console.log("logging to file: " + filename);
-        result(true);
+  setLogDirectory: function(path, result) {
+    if (fs.statSync(path).isDirectory()) {
+      // TODO: check for directory issues
+      dir = path;
+      updateLogFileList();
+      console.log("Directory found!");
+      result(null, true);
     } else {
-        result(false);
+      // TODO: throw an error
+      console.log("Directory is bad!");
+      result(null, false);
     }
+  },
+
+  getLogFileList: function(result) {
+    updateLogFileList();
+    result(null, fileList);
+  },
+
+  getLogFile: function(filename, result) {
+    if (fileList.indexOf(filename) > -1) {
+      result(null, fs.readFileSync(dir + '/' + filename));
+    } else {
+      throw FileNotFound;
+    }
+  },
+
+  startLogging: function(filename, result) {
+    if (!file) {
+      file = fs.createWriteStream(dir + '/' + filename, {
+        flags: 'w'
+      });
+      file.on('error', function(err) {
+        console.log("error opening file: " + err);
+        file.end();
+        file = false;
+      });
+      file.on('open', function() {
+        console.log("logging to file: " + filename);
+        initTime = getHrTimeMs();
+        fileList.push(filename);
+      });
+    }
+    result(null, true);
   },
 
   stopLogging: function(result) {
-    console.log("stopping logging to file")
-    file.end();
-    file = false;
-    result(true);
+    if (file) {
+      console.log("stopping logging to file");
+      file.end();
+      file = false;
+    }
+    result(null, true);
   },
 
-  log: function(level, mstime, message, result) {
-    // <add check that message is json here>
+  log: function(level, message) {
+    try {
+      message = JSON.parse(message)
+    } catch (e) {
+    }
     jsonmsg = {
-        'time': Date.now(),
-        'level': level,
-        'message': JSON.parse(message)
+      'time': getHrTimeMs() - initTime,
+      'level': level,
+      'message': message
     };
     message = JSON.stringify(jsonmsg);
-    if(file) {
-        file.write(message + '\n');
+    if (file) {
+      file.write(message + '\n');
     }
-    // console.log(message);
-    result(true);
   }
 };
 
 var server = thrift.createServer(Logger, LoggerHandler).on('error', function(err) {
-    // handle new errors as they arise
-    switch(err.code) {
-        case 'ECONNRESET':
-            console.log("Client abruptly disconnected! (ECONNRESET)");
-            break;
-        default:
-            console.log("Unknown error:\n\t" + err);
-            break;
-    }
+  // handle new errors as they arise
+  switch (err.code) {
+    case 'ECONNRESET':
+      console.log("Client abruptly disconnected! (ECONNRESET)");
+      break;
+    default:
+      console.log("Unknown error:\n\t" + err);
+      break;
+  }
 });
 
 console.log("Starting server");
--- logging/test.js
+++ logging/test.js
@@ -6,29 +6,43 @@ var connection = thrift.createConnection("localhost", 5001);
 client = thrift.createClient(Logger, connection);
 
 connection.on('error', function(err) {
-  assert(false, err);
+  console.log(err);
 });
 
+client.getLogFileList(function(err, response) {
+  console.log("File list: " + response);
+});
+
+client.setLogDirectory('/home/auvua/log', function(err, response) {
+  console.log(response);
+});
 
 client.startLogging('test.txt', function(err, response) {
   console.log('startLogging(test.txt)');
 });
 
 msg = {
-    'Test message': "does it work?",
-    'I think': 42,
-    'it does': 3.14159,
-    'x': 0
+  'time': Date.now(),
+  'Test message': "does it work?",
+  'I think': 42,
+  'it does': 3.14159,
+  'x': 0
 };
 
 x = 0;
 setInterval(function() {
-    msg['x'] = ++x;
-    client.log(ttypes.Level.INFO, 0, JSON.stringify(msg), function(err, response) {
-      console.log("logging data");
-    });
-    if(x > 1000) {
+  msg['x'] = ++x;
+  msg['time'] = Date.now();
+  client.log(ttypes.Level.INFO, JSON.stringify(msg), function(err, response) {
+    console.log("logging data");
+  });
+  if (x > 100) {
+    client.stopLogging(function(err, response) {
+      client.getLogFile('test.txt', function(err, response) {
+        console.log("File: " + response);
         connection.end();
         process.exit();
-    }
+      });
+    });
+  }
 }, 10);
